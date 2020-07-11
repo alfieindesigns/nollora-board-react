3f40a5908066b036a1f5d41bf1a92597fdf8e56f
Alfie Nollora <alfienollora@gmail.com>
2020-07-11 21:02:13 +0800

    Updates

---
+++ precache-manifest.8a34e4cae895f3e3df2377a971b9e503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b3666f1aff95a62fa9b4a58d12a4d4f",
+    "url": "/nollora-board-react/index.html"
+  },
+  {
+    "revision": "e32a11fa81947c24413f",
+    "url": "/nollora-board-react/static/css/main.d2a2d668.chunk.css"
+  },
+  {
+    "revision": "f506374cd4f2a4882842",
+    "url": "/nollora-board-react/static/js/2.fac95660.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nollora-board-react/static/js/2.fac95660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e32a11fa81947c24413f",
+    "url": "/nollora-board-react/static/js/main.72d22383.chunk.js"
+  },
+  {
+    "revision": "0756c0278f76122355e5",
+    "url": "/nollora-board-react/static/js/runtime-main.b9c0be06.js"
+  }
+]);
